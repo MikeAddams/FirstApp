eb8f9b2b76f79dc1dfebc08f18a9b52eb1fc577f
Florin Turcanu <Florin.Turcanu@endava.com>
2021-03-08 15:11:00 +0200

    Add project files.

---
+++ App/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace App.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string username, string password, string returnUrl)
+        {
+            if ((username == "Admin") && (password == "Admin"))
+            {
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, username)
+                };
+                var claimsIdentity = new ClaimsIdentity(claims, "Login");
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                return Redirect("/Account/Profile");
+            }
+
+            return View();
+        }
+    }
+}
