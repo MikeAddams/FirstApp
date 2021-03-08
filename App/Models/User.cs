7b13179bfeca38abf5e368a0bb3699ed1dad0753
Florin Turcanu <Florin.Turcanu@endava.com>
2021-03-08 18:32:59 +0200

    Added RegisterViewModel

--- App/Models/User.cs
+++ App/Models/User.cs
@@ -12,6 +12,8 @@ namespace App.Models
         public string Email { get; set; }
         public string Password { get; set; }
 
-        public string Name { get; set; }   
+        public string Nickname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
---
+++ App/Models/View/RegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Models.View
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "There's no username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "There's no email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "There's no password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "There's no nickname")]
+        public string Nickname { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
